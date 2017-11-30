13378b0e8a72a239da3c2ac8b1935b395ccb243b
icevct <icevct@gmail.com>
2017-11-30 17:52:01 -0200

    escopo do script e mais atualizacao do txt

---
+++ projetofinal.m
@@ -0,0 +1,12 @@
+%% Projeto final de PVC - 2/2017 
+%Andre Luis Souto Ferreira - 140016261
+%Victor Araujo Vieira - 14/0032801
+
+%% Script que executa os comandos necessarios para aplicar o modelo deepbit treinado nas imagens de insetos
+% Sera dividido em 4 partes: Inicializacao dos dados, extracao dos
+% descritores binarios para todas imagens(dividindo as imagens em treino e teste),
+% treinamento da SVM e, por ultimo, o teste da eficiencia do classificador.
+
+%% Inicializacao dos dados
+% Parte do script que vai preparar os dados
+
